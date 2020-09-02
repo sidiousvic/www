f6b6859f9a70e0faee5f880a536cfadd75ce62a5
sidiousvic <victor6rene@gmail.com>
2020-09-03 00:51:16 +0900

    remove deploy script

--- @.js
+++ @.js
@@ -1,10 +1,5 @@
 const express = require("express");
-const deploy = require("./deploy");
-const bodyParser = require("body-parser");
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/ui"));
 
@@ -12,10 +7,8 @@ app.get("/", (_, res) => {
   res.sendFile(__dirname + "/ui/index.html");
 });
 
-app.post("/webhooks/github", deploy);
-
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
-  console.log(`Server running @ PORT ${port} 🚢💨`);
+  console.log(`Server running @ PORT ${port}`);
 });
