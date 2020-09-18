93dafdc6d0b1a2d6169ca478e215017c6503b084
sidiousvic <sidiousvic@gmail.com>
2020-09-18 07:18:33 +0000

    rnm @.js > server.js

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + "/dist"));
-
-app.get("/", (_, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
-});
-
 app.get("/%F0%9F%8D%B5", (_, res) => {
   res.status(418);
   res.send("🍓𝟙𝒆 ");
 });
 
-const port = process.env.PORT || 9000;
+const port = 9999;
 
 app.listen(port, () => {
   console.log(`Server running @ PORT ${port}`);
