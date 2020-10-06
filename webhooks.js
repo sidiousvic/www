86aee8070045852e724cf8c7bf4bc0b65677ae74
sidiousvic <sidiousvic@gmail.com>
2020-10-06 15:02:22 +0900

    upd status res

--- webhooks.js
+++ webhooks.js
@@ -14,9 +14,9 @@ webhookRouter.post("/build/:service", async (req, res) => {
   const { service } = req.params;
   if (ref.indexOf("prod") > -1 && sender.login === githubUsername) {
     console.log(`🔩 Running ${service} deploy script...`);
-    res.sendStatus(200).send("🔧 Deploy has been triggered. ");
+    res.status(200).send("🔧 Deploy has been triggered. ");
     deploy(service);
-  } else res.sendStatus(500).send("😵 Deploy was unable to trigger. ");
+  } else res.status(500).send("😵 Deploy was unable to trigger. ");
 });
 
 function deploy(service) {
