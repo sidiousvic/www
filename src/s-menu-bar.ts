d89d845b31a95c5cd9b1d3f2d23d7a04df896070
sidiousvic <sidiousvic@gmail.com>
2020-10-06 20:21:07 +0900

    copy upd

--- src/s-menu-bar.ts
+++ src/s-menu-bar.ts
@@ -8,7 +8,7 @@ class sMenuBar extends HTMLElement {
     this.innerHTML = this.innerHTML;
   }
 
-  items: string[] = ["THIS WWWu IS UNDER CONSTRUCTION 🏴‍☠️ GITHUB/SIDIOUSVIC"];
+  items: string[] = ["THIS WWWX IS UNDER CONSTRUCTION 🏴‍☠️ GITHUB/SIDIOUSVIC"];
 
   innerHTML = html`
     <div class="img" id="kermitoid-logo"></div>
